e0062ca533ad038d46de73f603e4dfb1b775136f
Nicholas Miller <nickbryanmiller@gmail.com>
2016-01-20 15:40:40 -0500

    Added information about merge sort and started implementing Heap Sort. I also fixed the printing of the array issue because I can't math 😊

--- Sorting Algorithms/SortingAlgorithmsXcode/SortingAlgorithmsXcode/main.cpp
+++ Sorting Algorithms/SortingAlgorithmsXcode/SortingAlgorithmsXcode/main.cpp
@@ -25,16 +25,16 @@ using namespace std;
 
 int size;
 
-// Properties of the Insertion Sort
-// Stable:              Yes
-// Extra Space:         Θ(1)
-// Overall Complexity:  Θ(n^2)
-// Worst-Case:          Θ(n^2)
-// Best-Case:           Θ(n)
-// Adaptive:            Yes, Θ(n) when nearly sorted
-// Overhead:            Very Low
-void insertionSort(int array[], int size) {
-    for (int i = 0; i < size - 1; i++) {
+// Properties of the Bad Insertion Sort
+// Stable:                  Yes
+// Extra Space (Array):     Θ(1)
+// Overall Complexity:      Θ(n^2)
+// Worst-Case:              Θ(n^2)
+// Best-Case:               Θ(n^2)
+// Adaptive:                Yes
+// Overhead:                Very Low
+void badInsertionSort(int array[], int size) {
+    for (int i = 0; i <= size - 1; i++) {
         for (int j = size - 1; j > 0; j--) {
             if (array[j] < array[j-1]) {
                 int temp = array[j];
@@ -45,17 +45,41 @@ void insertionSort(int array[], int size) {
     }
 }
 
-// Properties of the Bubble Sort
-// Stable:              Yes
-// Extra Space:         Θ(1)
-// Overall Complexity:  Θ(n^2)
-// Worst-Case:          Θ(n^2)
-// Best-Case:           Θ(n^2)
-// Adaptive:            Yes, Θ(^2) when nearly sorted
-// Overhead:            Very Low
+// Properties of the Good Insertion Sort
+// Stable:                  Yes
+// Extra Space (Array):     Θ(1)
+// Overall Complexity:      Θ(n^2)
+// Worst-Case:              Θ(n^2)
+// Best-Case:               Θ(n)
+// Adaptive:                Yes, Θ(n) when nearly sorted
+// Overhead:                Very Low
+// This algorithm is better and takes less steps than the one above.
+//  I am resubmitting because it is better.
+void goodInsertionSort(int array[], int size) {
+    int j;
+    
+    for (int i = 0; i <= size - 1; i++) {
+        j = i;
+        while (j > 0 && array[j] < array[j-1]) {
+            int temp = array[j];
+            array[j] = array[j-1];
+            array[j-1] = temp;
+            j--;
+        }
+    }
+}
+
+// Properties of the Bad Bubble Sort
+// Stable:                  Yes
+// Extra Space (Array):     Θ(1)
+// Overall Complexity:      Θ(n^2)
+// Worst-Case:              Θ(n^2)
+// Best-Case:               Θ(n^2)
+// Adaptive:                Yes, Θ(^2) when nearly sorted
+// Overhead:                Very Low
 void badBubbleSort(int array[], int size) {
     
-    for (int i = 0; (i < size - 1); i++) {
+    for (int i = 0; i <= size - 1; i++) {
         for (int j = 0; j < size - 1; j++) {
             if (array[j+1] < array[j]) {
                 int temp = array[j];
@@ -67,13 +91,13 @@ void badBubbleSort(int array[], int size) {
 }
 
 // Properties of the Modified Bubble Sort
-// Stable:              Yes
-// Extra Space:         Θ(1)
-// Overall Complexity:  Θ(n^2)
-// Worst-Case:          Θ(n^2)
-// Best-Case:           Θ(n)
-// Adaptive:            Yes, Θ(n) when nearly sorted
-// Overhead:            Low
+// Stable:                  Yes
+// Extra Space (Array):     Θ(1)
+// Overall Complexity:      Θ(n^2)
+// Worst-Case:              Θ(n^2)
+// Best-Case:               Θ(n)
+// Adaptive:                Yes, Θ(n) when nearly sorted
+// Overhead:                Low
 void forBubbleSort(int array[], int size) {
     bool sorted = false;
     
@@ -110,13 +134,13 @@ void whileBubbleSort(int array[], int size) {
 }
 
 // Properties of the Selection Sort
-// Stable:              No
-// Extra Space:         Θ(1)
-// Overall Complexity:  Θ(n^2)
-// Worst-Case:          Θ(n^2)
-// Best-Case:           Θ(n)
-// Adaptive:            No
-// Overhead:            Low
+// Stable:                  No
+// Extra Space (Array):     Θ(1)
+// Overall Complexity:      Θ(n^2)
+// Worst-Case:              Θ(n^2)
+// Best-Case:               Θ(n)
+// Adaptive:                No
+// Overhead:                Low
 void selectionSort(int array[], int size) {
     int minPos;
     int temp;
@@ -141,6 +165,16 @@ void selectionSort(int array[], int size) {
     }
 }
 
+// Properties of the Merge Sort
+// Stable:                      Yes
+// Extra Space (Array):         Θ(n)
+// Extra Space (Linked List):   Θ(log(n))
+// Overall Complexity:          Θ(n * log(n))
+// Worst-Case:                  Θ(n * log(n))
+// Best-Case:                   Θ(n * log(n))
+// Adaptive:                    No
+// Extra:                       Does not require random access to data
+/* leftIndex is for left index and rightIndex is right index of the sub-array of array to be sorted */
 void merge(int array[], int leftIndex, int middleIndex, int rightIndex) {
     int i;
     int j;
@@ -193,7 +227,6 @@ void merge(int array[], int leftIndex, int middleIndex, int rightIndex) {
     }
 }
 
-/* leftIndex is for left index and rightIndex is right index of the sub-array of array to be sorted */
 void mergeSort(int array[], int leftIndex, int rightIndex) {
     if (leftIndex < rightIndex) {
         //Same as (leftIndex + rightIndex)/2, but avoids overflow for large leftIndex and h
@@ -204,20 +237,72 @@ void mergeSort(int array[], int leftIndex, int rightIndex) {
     }
 }
 
+// Properties of the Heap Sort
+// Stable:                      No
+// Extra Space (Array):         Θ(1)
+// Overall Complexity:          Θ(n * log(n))
+// Worst-Case:                  Θ(n * log(n))
+// Best-Case:                   Θ(n * log(n))
+// Adaptive:                    No
+void maxHeap(int a[], int i, int n) {
+    int l,r,largest,loc;
+    l=2*i;
+    r=(2*i+1);
+    if((l<=n)&&a[l]>a[i])
+        largest=l;
+    else
+        largest=i;
+    if((r<=n)&&(a[r]>a[largest]))
+        largest=r;
+    if(largest!=i)
+    {
+        loc=a[i];
+        a[i]=a[largest];
+        a[largest]=loc;
+        maxHeap(a, largest,n);
+    }
+}
+void buildMaxHeap(int a[], int n) {
+    for(int k = n/2; k >= 1; k--)
+    {
+        maxHeap(a, k, n);
+    }
+}
+void heapSort(int a[], int n) {
+    
+    buildMaxHeap(a,n);
+    int i, temp;
+    for (i = n; i >= 2; i--)
+    {
+        temp = a[i];
+        a[i] = a[1];
+        a[1] = temp;
+        maxHeap(a, 1, i - 1);
+    }
+}
+
 int main(int argc, const char * argv[]) {
-    cout << "By the Insertion Sort Method\n";
+    cout << "By the Bad Insertion Sort Method\n";
     int myArray1[] = {3, 2, 5, 7, 4, 12, 6, 15, 1, 0, 8, 10, 11};
     size = sizeof(myArray1)/sizeof(int);
-    insertionSort(myArray1, size);
-    for (int i = 0; i < size - 1; i++) {
+    badInsertionSort(myArray1, size);
+    for (int i = 0; i <= size - 1; i++) {
         cout << myArray1[i] << "\t";
+    }
+    
+    cout << "\nBy the Good Insertion Sort Method\n";
+    int myArray7[] = {3, 2, 5, 7, 4, 12, 6, 15, 1, 0, 8, 10, 11};
+    size = sizeof(myArray7)/sizeof(int);
+    goodInsertionSort(myArray7, size);
+    for (int i = 0; i <= size - 1; i++) {
+        cout << myArray7[i] << "\t";
     }
     
     cout << "\nBy the Bad Bubble Sort Method\n";
     int myArray2[] = {3, 2, 5, 7, 4, 12, 6, 15, 1, 0, 8, 10, 11};
     size = sizeof(myArray2)/sizeof(int);
     badBubbleSort(myArray2, size);
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i <= size - 1; i++) {
         cout << myArray2[i] << "\t";
     }
     
@@ -225,7 +310,7 @@ int main(int argc, const char * argv[]) {
     int myArray3[] = {3, 2, 5, 7, 4, 12, 6, 15, 1, 0, 8, 10, 11};
     size = sizeof(myArray3)/sizeof(int);
     forBubbleSort(myArray3, size);
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i <= size - 1; i++) {
         cout << myArray3[i] << "\t";
     }
     
@@ -233,7 +318,7 @@ int main(int argc, const char * argv[]) {
     int myArray4[] = {3, 2, 5, 7, 4, 12, 6, 15, 1, 0, 8, 10, 11};
     size = sizeof(myArray4)/sizeof(int);
     whileBubbleSort(myArray4, size);
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i <= size - 1; i++) {
         cout << myArray4[i] << "\t";
     }
     
@@ -241,7 +326,7 @@ int main(int argc, const char * argv[]) {
     int myArray5[] = {3, 2, 5, 7, 4, 12, 6, 15, 1, 0, 8, 10, 11};
     size = sizeof(myArray5)/sizeof(int);
     selectionSort(myArray5, size);
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i <= size - 1; i++) {
         cout << myArray5[i] << "\t";
     }
     
@@ -249,8 +334,16 @@ int main(int argc, const char * argv[]) {
     int myArray6[] = {3, 2, 5, 7, 4, 12, 6, 15, 1, 0, 8, 10, 11};
     size = sizeof(myArray6)/sizeof(int);
     mergeSort(myArray6, 0, size - 1);
-    for (int i = 0; i < size - 1; i++) {
+    for (int i = 0; i <= size - 1; i++) {
         cout << myArray6[i] << "\t";
+    }
+    
+    cout << "\nBy the Heap Sort Method\n";
+    int myArray8[] = {3, 2, 5, 7, 4, 12, 6, 15, 1, 0, 8, 10, 11};
+    size = sizeof(myArray8)/sizeof(int);
+    heapSort(myArray8, size);
+    for (int i = 0; i <= size - 1; i++) {
+        cout << myArray8[i] << "\t";
     }
     
     
