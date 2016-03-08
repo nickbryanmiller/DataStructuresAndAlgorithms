7ec7561585c80d06d4177e318df4a105517c1103
Nicholas Miller <nickbryanmiller@gmail.com>
2016-03-08 15:25:57 -0500

    Fully implemented with stack

--- Data Structures/Calculator/CalculatorWithStack.cpp
+++ Data Structures/Calculator/CalculatorWithStack.cpp
@@ -1,6 +1,6 @@
 //
 //  main.cpp
-//  ScientificCalculator
+//  CalculatorWithHash
 //
 //  Created by Nicholas Miller on 2/29/16.
 //  Copyright © 2016 nickbryanmiller. All rights reserved.
@@ -14,8 +14,7 @@
 #include <sstream>
 #include <stdlib.h>
 #include <iterator>
-#include <unordered_map>
-#include <map>
+//#include <unordered_map>
 
 using std::string;
 using std::cout;
@@ -25,14 +24,188 @@ using std::queue;
 using std::stringstream;
 using std::istringstream;
 using std::endl;
-using std::unordered_map;
+//using std::unordered_map;
+using std::stoi;
+using std::stod;
 
 bool didDivideByZero = false;
 bool variableDoesNotExist = false;
 
+// This is just like our node class with a few slight changes
+class LinkedHashEntry {
+private:
+    string key;
+    double value;
+    LinkedHashEntry *next;
+public:
+    // This is our constructor and how we are going to make a new node for that particular bucket
+    LinkedHashEntry(string key, double value) {
+        this->key = key;
+        this->value = value;
+        this->next = NULL;
+    }
+    string getKey() {
+        return key;
+    }
+    string getValue() {
+        stringstream ss;
+        ss << value;
+        return ss.str();
+    }
+    void setValue(double value) {
+        this->value = value;
+    }
+    LinkedHashEntry *getNext() {
+        return next;
+    }
+    void setNext(LinkedHashEntry *next) {
+        this->next = next;
+    }
+};
+
+// Can store a default of 128 elements (2^7 = 8 bits to use);
+const int DEFAULT_TABLE_SIZE = 128;
+
+/*
+ * This is a basic implementation of a hashmap with chaining (a linked list at each array index bucket)
+ */
+class HashTable {
+private:
+    float threshold;
+    int maxSize;
+    int tableSize;
+    int size;
+    
+    // Double pointer because a LIST of entries is a table
+    LinkedHashEntry **table;
+    
+    // This is standard library method for hashing a string (returns a size_t)
+    //    std::hash<std::string> str_hash;
+    
+    // If we every reach 128 entries we make a new table double the original size and copy the old table over
+    void resize() {
+        int oldTableSize = tableSize;
+        tableSize *= 2;
+        maxSize = (int) (tableSize * threshold);
+        LinkedHashEntry **oldTable = table;
+        table = new LinkedHashEntry*[tableSize];
+        for (int i = 0; i < tableSize; i++) {
+            table[i] = NULL;
+        }
+        size = 0;
+        for (int hash = 0; hash < oldTableSize; hash++) {
+            if (oldTable[hash] != NULL) {
+                LinkedHashEntry *oldEntry;
+                LinkedHashEntry *entry = oldTable[hash];
+                while (entry != NULL) {
+                    put(entry->getKey(), stod(entry->getValue()));
+                    oldEntry = entry;
+                    entry = entry->getNext();
+                    delete oldEntry;
+                }
+            }
+        }
+        // Use the system deallocator function so we do not reach memory capacity
+        delete[] oldTable;
+    }
+    
+    // This method turns a string into a number so that we can do modulo tablesize to get it into a bucket
+    // This is the RSHash function
+    // It uses a and b (prime numbers) to manipulate the the numerical value of 'hash'
+    size_t hashMe(const std::string& str) {
+        size_t b = 378551;
+        size_t a = 63689;
+        size_t hash = 0;
+        
+        for(std::size_t i = 0; i < str.length(); i++) {
+            hash = hash * a + str[i];
+            a = a * b;
+        }
+        return hash;
+    }
+    
+    
+public:
+    HashTable() {
+        // Specify so that it is a float constant to the compiler
+        // 0.75 is recommended for max memory efficiency and time efficiency
+        threshold = 0.75f;
+        maxSize = 100;
+        tableSize = DEFAULT_TABLE_SIZE;
+        size = 0;
+        table = new LinkedHashEntry*[tableSize];
+        for (int i = 0; i < tableSize; i++) {
+            table[i] = NULL;
+        }
+    }
+    
+    // This sets the maximum size so we can check when we start reaching capacity
+    void setThreshold(float threshold) {
+        this->threshold = threshold;
+        maxSize = (int) (tableSize * threshold);
+    }
+    
+    // Returns the value associated with a key else returns 'NA'
+    string get(string key) {
+        // Modular tablesize is used to get the index because it is an even number and not many collisions occur
+        // It might be useful to find the nearest prime around tablesize instead
+        int hash = (hashMe(key) % tableSize);
+        if (table[hash] == NULL) {
+            return "Undeclared-Variable";
+        }
+        // If the position is not null we travel the linked list looking for the same key
+        else {
+            LinkedHashEntry *entry = table[hash];
+            while (entry != NULL && entry->getKey() != key) {
+                entry = entry->getNext();
+            }
+            if (entry == NULL) {
+                return "Undeclared-Variable";
+            }
+            else {
+                return entry->getValue();
+            }
+        }
+    }
+    
+    // This method sets a value for a particular key
+    void put(string key, double value) {
+        int hash = (hashMe(key) % tableSize);
+        
+        // If the index is null we make a new node in that bucket with the key/value pair
+        if (table[hash] == NULL) {
+            table[hash] = new LinkedHashEntry(key, value);
+            size++;
+        }
+        
+        // Else we search the bucket to make sure it does not exist before adding it
+        else {
+            LinkedHashEntry *entry = table[hash];
+            while (entry->getNext() != NULL && entry->getKey() != key) {
+                entry = entry->getNext();
+            }
+            if (entry->getKey() == key) {
+                entry->setValue(value);
+            }
+            else {
+                entry->setNext(new LinkedHashEntry(key, value));
+                size++;
+            }
+        }
+        
+        // If our size ever reaches the max size we double the table and copy the data over.
+        if (size >= maxSize) {
+            resize();
+        }
+    }
+};
+// Make a global hashmap so every method can use the same one
+// Called it ht because that is the name of my original hashtable implementation below
+HashTable ht;
+
 /* This is a basic node class
-*  It constains a string value and two node pointers to its two children
-*/
+ *  It constains a string value and two node pointers to its two children
+ */
 class Node{
 public:
     string data;
@@ -53,40 +226,40 @@ public:
 };
 
 /* This is an extension of the buit in unordered map
-*  There is two methods defined so the user can put a value associated with a key
-*  The other is so that you can return a value associated with a key
-*/
-class Hashtable {
-    std::unordered_map<string, double> htmap;
-    
-public:
-    
-    // If the key has no value then set a value else it should change the value at that key
-    void put(string key, double value) {
-        if (get(key) == "NA") {
-            htmap.insert({key,value});
-        }
-        else {
-            htmap.at(key) = value;
-        }
-    }
-    
-    // Returns the value of a key or NA if it does not exist
-    string get(string key) {
-        std::unordered_map<std::string,double>::const_iterator got = htmap.find(key);
-        if ( got == htmap.end() ) {
-            return "NA";
-        }
-        else {
-            stringstream ss;
-            ss << got->second;
-            return ss.str();
-        }
-    }
-    
-};
-// Make a global hashtable so every method can use the same one
-Hashtable ht;
+ *  There is two methods defined so the user can put a value associated with a key
+ *  The other is so that you can return a value associated with a key
+ */
+//class Hashtable {
+//    std::unordered_map<string, double> htmap;
+//
+//public:
+//
+//    // If the key has no value then set a value else it should change the value at that key
+//    void put(string key, double value) {
+//        if (get(key) == "NA") {
+//            htmap.insert({key,value});
+//        }
+//        else {
+//            htmap.at(key) = value;
+//        }
+//    }
+//
+//    // Returns the value of a key or NA if it does not exist
+//    string get(string key) {
+//        std::unordered_map<std::string,double>::const_iterator got = htmap.find(key);
+//        if ( got == htmap.end() ) {
+//            return "NA";
+//        }
+//        else {
+//            stringstream ss;
+//            ss << got->second;
+//            return ss.str();
+//        }
+//    }
+//
+//};
+//// Make a global hashtable so every method can use the same one
+//Hashtable ht;
 
 // This sets a precedence for each token you can encounter
 int getPrecedence(char token) {
@@ -228,10 +401,10 @@ queue<string> infixToPostfix(string infix) {
                 i++;
             }
             string vari = ht.get(postfix);
-            if (vari != "NA") {
+            if (vari != "Undeclared-Variable") {
                 myQueue.push(vari);
             }
-            else if (vari == "NA") {
+            else if (vari == "Undeclared-Variable") {
                 myQueue.push("0");
                 variableDoesNotExist = true;
             }
@@ -267,152 +440,78 @@ bool isOperator(string token) {
     }
 }
 
-// This will create a binary expression tree using a stack and the Node class
-Node* createTreeWithPostfix(string postfix[], int size) {
-    stack<Node*> st;
-    Node *root;
-    Node *right;
-    Node *left;
-    
-    // Go through every string in our postfix array
-    for (int i=0; i< size; i++) {
-        
-        // If it is an operand push it to the temp stack
-        if (!isOperator(postfix[i])) {
-            root = new Node(postfix[i]);
-            st.push(root);
-        }
-        // If it is an operator they should go to the right spot of the tree
-        else {
-            root = new Node(postfix[i]);
-            
-            // Pop the two top nodes and add them to the tree as the children
-            right = st.top();
-            st.pop();
-            left = st.top();
-            st.pop();
-            
-            //  make them children
-            root->rightChild = right;
-            root->leftChild = left;
-            
-            // Add this subexpression to stack
-            st.push(root);
-        }
-    }
-    
-    // The root should always be the top of the tree
-    root = st.top();
-    st.pop();
-    
-    return root;
-}
-
-// This will traverse the tree (sans parenthesis like in the powerpoint he showed) and print the infix to check
-void inorder(Node *t) {
-    if(t) {
-        inorder(t->leftChild);
-        cout << t->data << " ";
-        inorder(t->rightChild);
-    }
-}
-
+/* We can do the same evaluation as above but with a stack instead of constructing a tree
+ *  The stack computes the result faster than the tree because you don't need to contruct the unique tree
+ *  I did the tree just because I wanted to get experience with binary trees because they are a very
+ *  common in technical interviews and I am already comfortable with stacks
+ */
 // This is a method that does all of our operations
-double doMathFromTree(double a, double b, string op) {
+float evalFromStack(float op1, float op2, string op) {
     double answer = 0;
     
     if (op == "+") {
-        answer = a + b;
+        answer = op2 + op1;
     }
     else if (op == "-") {
-        answer = a - b;
+        answer = op2 - op1;
     }
     else if (op == "*") {
-        answer = a * b;
+        answer = op2 * op1;
     }
     else if (op == "/") {
         
         // If there is a division by zero flag that this occured and return a zero instead of 'inf'
-        if (b == 0) {
+        if (op1 == 0) {
             didDivideByZero = true;
             answer = 0;
         }
-        else if (b != 0) {
-            answer = a / b;
+        else if (op1 != 0) {
+            answer = op2 / op1;
         }
     }
     else if (op == "^") {
-        answer = pow(a, b);
+        answer = pow(op2, op1);
     }
-
+    
     return answer;
 }
 
-// The equation to evaluate the tree goes through and passes the parent and the two children to the math method
-double evaluatetree(Node *x){
-    if (x != NULL) {
-        
-        // If we are at a leaf we return that node's value. This is the base case
-        if (!isOperator(x->data)) {
-            return stod(x->data);
-        }
-        // If it is an operator we pass the two children to the math function recursively
-        else if (isOperator(x->data)) {
-            double a = evaluatetree(x->leftChild);
-            double b = evaluatetree(x->rightChild);
-            
-            return doMathFromTree(a, b, x->data);
-        }
+// This makes a stack and does the math based on that
+float evalPostfixWithStack(string postfix[], int size) {
+    stack<float> s;
+    int i = 0;
+    string ch;
+    float val = 0;
+    
+    if (size == 1) {
+        ch = postfix[0];
+        return std::stof(ch);
     }
-    return 0;
-}
+    
+    while (i < size) {
+        ch = postfix[i];
+        
+        // If it is an operand we push it to that stack
+        if (!isOperator(ch)) {
+            s.push(std::stof(ch));
+        }
+        
+        // If it is an operator we pop off the last two operands and call our stack math function with the operand
+        else {
+            float op1 = s.top();
+            s.pop();
+            float op2 = s.top();
+            s.pop();
+            val = evalFromStack(op1, op2, ch);
+            
+            // We push the result of the math onto the stack to be used in the next case
+            s.push(val);
+        }
+        i++;
+    }
 
-/* We can do the same evaluation as above but with a stack instead of constructing a tree
-*  The stack computes the result faster than the tree because you don't need to contruct the unique tree
-*  I did the tree just because I wanted to get experience with binary trees because they are a very
-*  common in technical interviews and I am already comfortable with stacks
-*/
-//// This is a method that does all of our operations
-//int evalFromStack(int op1, int op2, string operate) {
-//    switch (operate[0]) {
-//        case '*': return op2 * op1;
-//        case '/': return op2 / op1;
-//        case '+': return op2 + op1;
-//        case '-': return op2 - op1;
-//        default : return 0;
-//    }
-//}
-//
-//// This makes a stack and does the math based on that
-//int evalPostfixWithStack(string postfix[], int size) {
-//    stack<int> s;
-//    int i = 0;
-//    string ch;
-//    int val = 0;
-//
-//    while (i < size) {
-//        ch = postfix[i];
-//
-//        // If it is an operand we push it to that stack
-//        if (!isOperator(ch)) {
-//            s.push(std::stoi(ch));
-//        }
-//
-//        // If it is an operator we pop off the last two operands and call our stack math function with the operand
-//        else {
-//            int op1 = s.top();
-//            s.pop();
-//            int op2 = s.top();
-//            s.pop();
-//            val = evalFromStack(op1, op2, ch);
-//
-//            // We push the result of the math onto the stack to be used in the next case
-//            s.push(val);
-//        }
-//        i++;
-//    }
-//    return val;
-//}
+    return val;
+}
 
 int main(int argc, const char * argv[]) {
     
@@ -488,11 +587,8 @@ int main(int argc, const char * argv[]) {
                     i++;
                 }
                 
-                // Construct the binary expression tree and return the root node
-                Node *root = createTreeWithPostfix(postfixArray, size);
-                
                 // Compute the answer of the binary expression tree
-                double answer = evaluatetree(root);
+                double answer = evalPostfixWithStack(postfixArray, size);
                 
                 // If the result is not a division by zero and the user is not setting a variable print the answer
                 if (infix.substr(0,3) != "let" && !didDivideByZero) {
